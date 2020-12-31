b6a0b68f8a695e4c483628a7cc77a22884047a3d
yakupgeyik96 <yakupgeyik537@gmail.com>
2020-12-31 19:52:01 +0300

    new commit

--- src/dbmsproj/Main.java
+++ src/dbmsproj/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("views/MainPage.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("PAZAR BİLGİ SİSTEMİ");
         primaryStage.setScene(new Scene(root, 1200, 800));
         primaryStage.setResizable(false);
         primaryStage.show();
--- src/dbmsproj/service/PostgreSqlConnection.java
+++ src/dbmsproj/service/PostgreSqlConnection.java
@@ -9,7 +9,7 @@ public class PostgreSqlConnection {
 
     private final String DB_URL = "jdbc:postgresql://localhost/company_db";
     private final String DB_USERNAME = "postgres";
-    private final String DB_PASSWORD = "Lx0ex7ox5Tbpyu8h1!9";
+    private final String DB_PASSWORD = "*******************";
 
     private static PostgreSqlConnection instance = null;
 
